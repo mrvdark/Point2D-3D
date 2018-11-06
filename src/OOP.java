dd4a9ebdff215b63e2680a7a9129ced616becbdc
mrvdark <m.r.v.leo.dark@gmail.com>
2018-11-06 10:25:52 +0700

    done

---
+++ src/OOP.java
@@ -0,0 +1,13 @@
+
+
+public class OOP {
+
+    public static void main(String[] args) {
+        D2 p2 = new D2(5, 7);
+        d3 p3 = new d3(3, 7, 8);
+
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+    }
+
+}
